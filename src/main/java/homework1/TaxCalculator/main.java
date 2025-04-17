d88341644e91296bbe489a6747cf7ce57107b1f4
prinja <pavel.rinja@orange.com>
2025-04-18 02:11:05 +0300

    Homework 1

---
+++ src/main/java/homework1/TaxCalculator/main.java
@@ -0,0 +1,25 @@
+package homework1.TaxCalculator;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        double price = 99.9;
+        double tax = 1.33;
+        int quantity;
+        double total = 0;
+        String message = "I want to buy X shirt!";
+
+        System.out.println(message);
+
+        // Scan quantity which we want to buy
+        Scanner in = new Scanner(System.in);
+        quantity = in.nextInt();
+
+        // Total cost formula
+        total = price * quantity * tax;
+
+        System.out.println("Total cost with tax is: " + total);
+    }
+}
