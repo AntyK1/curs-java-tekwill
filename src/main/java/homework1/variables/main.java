7a908858f4f8e1c8186adebfcf45f370cb005bc6
prinja <pavel.rinja@orange.com>
2025-04-18 01:34:35 +0300

    test

---
+++ src/main/java/homework1/variables/main.java
@@ -0,0 +1,18 @@
+package homework1.variables;
+
+public class main {
+    public static void main(String[] args) {
+
+        long currentWorldPopulation = 7811080815L;
+        int carsProducedThisYear = 55235358;
+        char firstLetterInAlphabet = 'A';
+        boolean javaIsAmazing = true;
+        double Pi = 3.14159;
+
+    System.out.println("Current World Population is " + currentWorldPopulation);
+    System.out.println(carsProducedThisYear + " Cars Produced This Year");
+    System.out.println("First Letter In Alphabet is " + firstLetterInAlphabet);
+    System.out.println("Java Is Amazing - " + javaIsAmazing);
+    System.out.println("Pi: " + Pi);
+    }
+}
